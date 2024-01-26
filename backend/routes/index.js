b5b8d759af805ae3da87360c41d7de797e788200
Jasmine <jasminetruong97@gmail.com>
2024-01-26 10:31:38 -0800

    Add User input validation on user login backend endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,24 +4,24 @@ const router = express.Router();
 
 const apiRouter = require('./api');
 
-router.use('/api', apiRouter);
 
 //test route
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
 
 // backend/routes/index.js
 // ...
 // Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
-  // ...
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     'XSRF-Token': csrfToken
+//   });
+// });
+// ...
 
+router.use('/api', apiRouter);
 module.exports = router;
