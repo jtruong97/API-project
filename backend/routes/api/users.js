b9b3f500414185399977e4efe7f7777a2fdecb9a
Jasmine <jasminetruong97@gmail.com>
2024-01-30 08:24:03 -0800

    validate signup fN lN

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,8 @@
 // backend/routes/api/users.js
 const express = require('express')
 const bcrypt = require('bcryptjs');
-
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
