60d8458bb474c9c1e9ff2722f772a601571b2f67
Jasmine <jasminetruong97@gmail.com>
2024-01-30 10:04:37 -0800

    options debug

--- backend/db/migrations/20240129175200-create-spot.js
+++ backend/db/migrations/20240129175200-create-spot.js
@@ -71,6 +71,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Spots'
-    await queryInterface.dropTable(options);
+    await queryInterface.dropTable('Spots');
   }
 };
--- backend/db/migrations/20240129175741-create-review.js
+++ backend/db/migrations/20240129175741-create-review.js
@@ -51,6 +51,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Reviews'
-    await queryInterface.dropTable(options);
+    await queryInterface.dropTable('Reviews');
   }
 };
