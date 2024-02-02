d81e429bc05cb899fd23a95945c90f713833ce92
Jasmine <jasminetruong97@gmail.com>
2024-02-01 22:24:00 -0800

    debug all endpoints

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -6,29 +6,24 @@ const { check } = require('express-validator');
 
 const router = express.Router();
 
+
 router.delete('/:imageId', async (req,res) => {
-    try{
-        let { imageId } = req.params;
-        let userId = req.user.id;
+    let { imageId } = req.params;
+    let userId = req.user.id;
 
-        let spotImg = await SpotImage.findByPk(imageId)
-        if(!spotImg){
-            return res.status(404).json({ "message": "Spot Image couldn't be found" })
-        }
-        //console.log('spotImg', spotImg)
-        let spot = await Spot.findByPk(spotImg.spotId)
-        //console.log('SPOT',spot) // <-- null
-        if(spot.ownerId !== userId){
-            return res.status(400).json({'message':'This spot must belong to the current user'})
-        }
+    let spotImg = await SpotImage.findByPk(imageId,{attributes:['id','spotId','url','preview']})
+    if(!spotImg){
+        return res.status(404).json({ "message": "Spot Image couldn't be found" })
+    }
 
-        await spotImg.destroy();
-        return res.json({ "message": "Successfully deleted" })
+    let spot = await Spot.findByPk(spotImg.spotId)
+
+    if(spot.ownerId !== userId){
+        return res.status(403).json({'message':'This spot must belong to the current user'})
     }
-    catch(error){
-        return res.json(console.log(error))
-    }
+
+    await spotImg.destroy();
+    return res.json({ "message": "Successfully deleted" })
 })
-
 
 module.exports = router;
